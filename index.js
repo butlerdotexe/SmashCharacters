863b7aa35d46906e30f6008a2ffbf5a24f2a26da
Hoang Nguyen <hn374@drexel.edu>
2019-02-19 21:09:57 -0500

    Added placeholder for index render html get request

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+// Server is listening to port 8000
 app.listen(8000, function() {
 	console.log("Server has started...")
+});
+
+// Set to main index page, don't have file yet
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname + "/index.html"))
 });
