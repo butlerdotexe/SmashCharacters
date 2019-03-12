f64da45630b72015c6e10eb5bd7a9378c8571833
Hoang Nguyen <hn374@drexel.edu>
2019-03-11 23:17:40 -0400

    Added database connection and endpoint in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 var path = require('path');
 
 // Server is listening to port 8000
@@ -8,7 +8,36 @@ app.listen(8000, function() {
 	console.log("Server has started...")
 });
 
+// Connecting to sql database with help of the mysql library 
+var con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'databaseName' //Enter database name here
+});
+
+// Connecting to mysql database and error checking
+con.connect(function(err) {
+	if (err) {
+		console.log(err);
+		console.log("Error connecting to database.");
+	} else {
+		console.log("Database successfully connected.");
+	}
+});
+
 // Router set to main index page
 app.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname + "/index.html"))
 });
+
+// Endpoint to get table data from sql database
+/* app.get("/smash", function(req, res) {
+	con.query('SELECT * FROM ', function(err, rows, fields) { // Run the query to get data
+		if (err) {
+			console.log('Error during query processing.');
+		} else { // Success case
+			res.send(rows); // Send back the data from query
+		}
+	});
+}); */
